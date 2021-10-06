443dec60a7c1cbcf16879b59e38b7cda778dddb8
sanjeevjayasurya <sanjeev11398@gmail.com>
2021-10-06 23:57:58 +0530

    feat: add business logic to app(conversion logic)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,10 +25,30 @@ class MyAppState extends State<MyApp> {
     'pounds (lbs)',
     'ounces'
   ];
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
   String _startMeasure = '';
   String _convertMeasure = '';
-  final TextStyle inputStyle =
-      TextStyle(fontSize: 20, color: Colors.black);
+  String _resultMessage = '';
+  final TextStyle inputStyle = TextStyle(fontSize: 20, color: Colors.black);
   final TextStyle typography = TextStyle(fontSize: 24, color: Colors.grey[700]);
 
   @override
@@ -38,11 +59,21 @@ class MyAppState extends State<MyApp> {
     super.initState();
   }
 
-  // bool operator ==(dynamic other) =>
-  //     other != null && other is TimeSelection && this.hour == other.hour;
-  //
-  // @override
-  // int get hashCode => super.hashCode;
+  void convert(double value, String from, String to) {
+    int? nFrom = _measuresMap[from];
+    int? nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+    if (result == 0) {
+      _resultMessage = 'Choose both fields and enter value to continue';
+    } else {
+      _resultMessage =
+          '${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,8 +146,20 @@ class MyAppState extends State<MyApp> {
                   },
                 ),
                 Spacer(),
+                ElevatedButton(
+                  child: Text('Convert', style: inputStyle),
+                  onPressed: () {
+                    if (_startMeasure.isEmpty ||
+                        _convertMeasure.isEmpty ||
+                        _numberFrom == 0) {
+                      return;
+                    } else {
+                      convert(_numberFrom, _startMeasure, _convertMeasure);
+                    }
+                  },
+                ),
                 Text(
-                  _numberFrom.toString(),
+                  _resultMessage,
                   style: typography,
                 ),
                 Spacer()
